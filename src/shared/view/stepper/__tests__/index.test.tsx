31b6c349760f37566a0df112ee5eecefcc2fb73a
lordszn3 <robertkuzhin@gmail.com>
2023-06-01 11:07:30 +0500

    :white_check_mark: (view) add basic tests for stepper

---
+++ src/shared/view/stepper/__tests__/index.test.tsx
@@ -0,0 +1,54 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+
+import { Stepper } from '..'
+
+vi.spyOn(document, 'querySelector').mockImplementation(
+  () =>
+    ({
+      content: ''
+    } as HTMLMetaElement)
+)
+
+describe('stepper', () => {
+  const selectors = {
+    stepper: () => screen.findByRole('spinbutton'),
+    increase: () => screen.findByLabelText('Increase value'),
+    decrease: () => screen.findByLabelText('Decrease value')
+  }
+
+  it('should be increase value by arrow up pressed', async () => {
+    render(<Stepper value={10} step={1} min={0} max={100} />)
+
+    const stepper = await selectors.stepper()
+
+    expect(stepper).toHaveValue('10')
+
+    fireEvent.keyDown(stepper, { key: 'ArrowUp' })
+
+    expect(stepper).toHaveValue('11')
+  })
+
+  it('should be switch value to max by pressed End', async () => {
+    render(<Stepper value={10} step={1} min={0} max={100} />)
+
+    const stepper = await selectors.stepper()
+
+    expect(stepper).toHaveValue('10')
+
+    fireEvent.keyDown(stepper, { key: 'End' })
+
+    expect(stepper).toHaveValue('100')
+  })
+
+  it('should be switch value to min by pressed Home', async () => {
+    render(<Stepper value={10} step={1} min={0} max={100} />)
+
+    const stepper = await selectors.stepper()
+
+    expect(stepper).toHaveValue('10')
+
+    fireEvent.keyDown(stepper, { key: 'Home' })
+
+    expect(stepper).toHaveValue('0')
+  })
+})

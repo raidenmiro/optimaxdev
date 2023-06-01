1f12d15f325d5a2cbd1596d710d4d58d065d5cf7
lordszn3 <robertkuzhin@gmail.com>
2023-06-01 13:05:45 +0500

    :lipstick: (view) add base layout

---
+++ src/shared/layouts/base/index.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from 'react'
+
+import s from './index.module.css'
+
+interface ChildrenProps {
+  children: ReactNode
+}
+
+function Root({ children }: ChildrenProps) {
+  return <main className={s.layout}>{children}</main>
+}
+
+function Cart({ children }: ChildrenProps) {
+  return <section className={s.cart}>{children}</section>
+}
+
+function Summary({ children }: ChildrenProps) {
+  return <section className={s.summary}>{children}</section>
+}
+
+export const Layout = Object.assign(Root, {
+  Summary,
+  Cart
+})

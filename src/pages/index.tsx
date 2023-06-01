dec0e7f575dd66888730fc613073ca96fff4a211
lordszn3 <robertkuzhin@gmail.com>
2023-06-01 20:24:21 +0500

    :tada: (pages) add basic main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { CartSample } from '~/entities/cart/view'
+import { Layout } from '~/shared/layouts/base'
+import { Card } from '~/shared/view/card'
+
 export function Page() {
-  return <>hello</>
+  return (
+    <Layout>
+      <Layout.Cart>
+        <Card.Body>
+          <CartSample />
+        </Card.Body>
+      </Layout.Cart>
+      <Layout.Summary>summary</Layout.Summary>
+    </Layout>
+  )
 }

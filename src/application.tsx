12cf44c158a7fda5be345edd2eff99bc930c69db
lordszn3 <robertkuzhin@gmail.com>
2023-06-01 13:14:01 +0500

    :tada: (app) add redux store

--- src/application.tsx
+++ src/application.tsx
@@ -1,11 +1,14 @@
 import './base/index.css'
 
+import { Provider as ReduxProvider } from 'react-redux'
+
 import { Page } from './pages'
+import { store } from './shared/lib/redux-std/store'
 
 export function Application() {
   return (
-    <>
+    <ReduxProvider store={store}>
       <Page />
-    </>
+    </ReduxProvider>
   )
 }

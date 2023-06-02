180ef6ea5e3e9ae08cf8e3cbaa1def0542a0cf25
lordszn3 <robertkuzhin@gmail.com>
2023-06-02 11:29:35 +0500

    :arrow_up: (entities) add possible provides props

--- src/entities/cart/view/sample/index.tsx
+++ src/entities/cart/view/sample/index.tsx
@@ -1,19 +1,32 @@
+import type { ProductEntity } from '~/shared/api/schema'
 import { Badge } from '~/shared/view/badge'
 import { View } from '~/shared/view/generic'
 
 import s from './index.module.css'
 
-export function CartSample() {
+export type CartSampleProps = Pick<
+  ProductEntity,
+  'name' | 'image' | 'category' | 'promotions'
+>
+
+export function CartSample({
+  name,
+  image,
+  category,
+  promotions
+}: CartSampleProps) {
   return (
     <View className={s.cart_item}>
       <View className={s.cart_preview}>
-        <View as="img" src="" alt="" />
+        <View as="img" src={image} alt="preview product" />
       </View>
       <View className={s.cart_info}>
-        <View as="h2">T-shirt Unisex</View>
-        <View as="span">23% Stocks remaining</View>
+        <View as="h2">{name}</View>
+        <View as="span">{category}</View>
         <View className={s.cart_stocks}>
-          <Badge label="Shopping discount" />
+          {promotions.map((promotion) => (
+            <Badge key={promotion} label={promotion} />
+          ))}
         </View>
       </View>
     </View>

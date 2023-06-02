3a4e121d0f9e52428310392b8623e13450fe3a7a
lordszn3 <robertkuzhin@gmail.com>
2023-06-02 12:25:17 +0500

    :construction: (pages) add cart item (wip)

---
+++ src/entities/cart/view/item/index.tsx
@@ -0,0 +1,50 @@
+import cc from 'classcat'
+
+import type { ProductEntity } from '~/shared/api/schema'
+import type { MergeTypes } from '~/shared/lib/types'
+import { Checkbox } from '~/shared/view/checkbox'
+import { View } from '~/shared/view/generic'
+
+import { CartSample, Quantity } from '..'
+import s from './index.module.css'
+
+export function CartItem({
+  name,
+  quantity,
+  stockQuantity,
+  category,
+  promotions,
+  currency,
+  image,
+  price
+}: MergeTypes<ProductEntity, { currency: string }>) {
+  const formattedPrice = `${price}${currency}`
+
+  return (
+    <View className={s.container}>
+      <View className={s.column}>
+        <Checkbox />
+      </View>
+
+      <View className={s.column}>
+        <CartSample
+          category={category}
+          image={image}
+          promotions={promotions}
+          name={name}
+        />
+      </View>
+
+      <View className={s.column}>
+        <Quantity maxQuantity={stockQuantity} quantity={quantity} />
+      </View>
+
+      <View className={cc([s.total, s.column])}>
+        <View as="span">{formattedPrice}</View>
+        <View as="span" className="sr-only">
+          {currency}
+        </View>
+      </View>
+    </View>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { CartSample } from '~/entities/cart/view'
+import { CartItem } from '~/entities/cart/view/item'
 import { Layout } from '~/shared/layouts/base'
 import { Card } from '~/shared/view/card'
 
@@ -7,7 +7,17 @@ export function Page() {
     <Layout>
       <Layout.Cart>
         <Card.Body>
-          <CartSample />
+          <CartItem
+            category="Mens Shirts"
+            currency="$"
+            image="https://via.placeholder.com/150"
+            id="1"
+            name="Mens T-Shirt"
+            price={20}
+            quantity={1}
+            stockQuantity={10}
+            promotions={['Shopping Sale', '20% off']}
+          />
         </Card.Body>
       </Layout.Cart>
       <Layout.Summary>summary</Layout.Summary>

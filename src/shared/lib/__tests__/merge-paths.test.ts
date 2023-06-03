120c3dbe0205e866c7ae0fe78a437f4531d114ae
lordszn3 <robertkuzhin@gmail.com>
2023-06-03 22:30:36 +0500

    :white_check_mark: (lib) add tests for `trimByPattern`

--- src/shared/lib/__tests__/merge-paths.test.ts
+++ src/shared/lib/__tests__/merge-paths.test.ts
@@ -1,4 +1,5 @@
 import { mergePaths } from '../merge-path'
+import { trimByPattern } from '../string'
 
 describe('merge paths', () => {
   it('should be concat with root path', () => {
@@ -19,5 +20,17 @@ describe('merge paths', () => {
     })
 
     expect(paths.someEndPoint()).toBe('root/someEndPoint')
+  })
+})
+
+describe('trimByPattern', () => {
+  it('should be trim by pattern', () => {
+    const string = '$$$$test$'
+    expect(trimByPattern(string, '$')).toBe('test')
+  })
+
+  it("should be doesn't change string", () => {
+    const string = 'test'
+    expect(trimByPattern(string, '$')).toBe('test')
   })
 })
